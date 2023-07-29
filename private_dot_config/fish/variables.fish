d1822b92ff7dbb534b02d43d3638e65367b6cc51
Peter Gumeson <gumeson@gmail.com>
2023-07-29 01:14:23 -0600

    Refactor run scripts

--- private_dot_config/fish/variables.fish
+++ private_dot_config/fish/variables.fish
@@ -5,9 +5,5 @@ set -x GIT_EDITOR nvim
 # disable homebrew github api https://github.com/Homebrew/brew/issues/93
 set -x HOMEBREW_NO_GITHUB_API 1
 
-set -x NIB_CRYPT_BUCKET_NAME tk-secrets
-
-set -x MUSIC_DIR ~/Music/iTunes/iTunes Music/Music/
-
 # better pager in pgcli https://www.pgcli.com/pager
 set -x LESS -XFR
