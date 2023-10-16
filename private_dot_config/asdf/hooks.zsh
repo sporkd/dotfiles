08f4c1b0366e8f1c6f65e1a884e57f2ca6a6d8e4
Peter Gumeson <gumeson@gmail.com>
2023-10-15 18:30:05 -0600

    Use adsf instad of pkgx for ruby

---
+++ private_dot_config/asdf/hooks.zsh
@@ -0,0 +1,44 @@
+# prepend asdf to path if .tool_versions
+function _asdf_chpwd() {
+  emulate -L zsh
+
+  if [ -f ".tool-versions" -a "$PWD" != "$HOME" ]; then
+    if command -v pkgx &> /dev/null; then
+      pkgx unload &> /dev/null
+    fi
+
+    if [ -f "$HOMEBREW_PREFIX/opt/asdf/libexec/asdf.sh" ]; then
+      source $HOMEBREW_PREFIX/opt/asdf/libexec/asdf.sh
+      plugins="$(asdf plugin list)"
+      while IFS= read -r line; do
+        [[ $line =~ ^[[:blank:]]*(#.*)?$ ]] && continue
+        read -rA parts <<<"$line"
+        plugin=$parts[1]
+        version=$parts[2]
+        if [[ $plugin && $version ]]; then
+          echo
+          if ! grep -q $plugin <<< "$plugins" ; then
+            echo "Adding $plugin plugin..."
+            asdf plugin-add $plugin
+            [ $? -ne 0 ] && continue
+          else
+            echo "Using $plugin plugin..."
+          fi
+          asdf list $plugin $version &> /dev/null
+          if [ $? -ne 0 ]; then
+            versions="$(asdf list all $plugin)"
+            if grep -q $version <<< "$versions" ; then
+              echo "installing $plugin $version..."
+              asdf install $plugin $version
+            else
+              echo "No compatible version found for $plugin $version"
+            fi
+          else
+            echo "$plugin $version already installed"
+          fi
+        fi
+      done < "$PWD/.tool-versions"
+    fi
+  fi
+}
+chpwd_functions=(${chpwd_functions[@]} "_asdf_chpwd")
